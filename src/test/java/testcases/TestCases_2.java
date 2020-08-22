5fbc869c94856f9568727ee8df455a018f474bbe
akashdktyagi <akashdktyagi@gmail.com>
2020-08-22 22:13:49 +0530

    added test case

--- src/test/java/testcases/TestCases_2.java
+++ src/test/java/testcases/TestCases_2.java
@@ -5,7 +5,10 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,16 +22,34 @@ public class TestCases_2 extends TestBase {
     }
 
     @Test
-    public void t_02_check_website_is_working(){
+    public void t_02_search_for_() throws InterruptedException {
         driver.get(base_url);
         String expected = "Personalized Holiday Packages | Customized Tour Packages from multiple local and trusted travel agents";
         String actual =driver.getTitle();
         Assert.assertEquals("Page Title validation",expected,actual);
+
+        WebDriverWait webDriverWait = new WebDriverWait(driver,20);
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Type a Destination']")));
+
+        Thread.sleep(20000);
+
+        //Select destinations
+        driver.findElement(By.xpath("//input[@placeholder='Type a Destination']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Type a Destination']")).sendKeys("Kerala");
+        driver.findElement(By.xpath("//button[text()='Kerala']")).click();
+
+        driver.findElement(By.xpath("//input[@value='Select duration']")).click();
+        driver.findElement(By.xpath("//label[@for='inputDays_1-3']")).click();
+        driver.findElement(By.xpath("//input[@value='Select month']")).click();
+        driver.findElement(By.xpath("//button[text()='November, 2020']")).click();
+        driver.findElement(By.xpath("//button[@class='at_explore_btn btn-filled-pri-large wfull ripple']")).click();
+
+
     }
 
     @After
     public void clean_up(){
-        driver.quit();
+        //driver.quit();
     }
 
 }
